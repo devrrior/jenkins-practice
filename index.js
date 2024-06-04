f1a76e67b4f2d0d421cd4cc622120a3ffa35bcc3
Fernando Guerrero <devrrior@gmail.com>
2024-06-04 00:42:01 -0600

    change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hola mundo!');
+  res.send('Hello world!');
 });
 
 app.listen(3000, () => {
