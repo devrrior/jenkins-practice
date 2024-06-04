160fb9457ba6ffc62a24a71433b04678fe09d70f
Fernando Guerrero <fernando@Fernandos-MacBook-Air.local>
2024-06-03 22:39:06 -0600

    my first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/v1/welcome', (req, res) => {
+  res.send('Hello, World from Express.js!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
