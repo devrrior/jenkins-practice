535613a964ea5a6f957827eaee3fd82b7c5ed5a2
Fernando Guerrero <devrrior@gmail.com>
2024-06-06 00:20:09 -0600

    fix

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ const express = require("express");
 
 const app = express();
 app.get("/api/v1/welcome", (req, res) => {
-  res.status(200).send("Hello, world!");
+  res.status(200).send("Hello, world1!");
 });
 
 describe("GET /api/v1/welcome", () => {
   it("should return welcome message", (done) => {
-    request(app).get("/api/v1/welcome").expect("Hello, world!").end(done);
+    request(app).get("/api/v1/welcome").expect("Hello, world!1").end(done);
   });
 });
