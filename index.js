66427a0d341422dafe802d1bf39f642c52bcd764
Fernando Guerrero <devrrior@gmail.com>
2024-06-06 00:24:34 -0600

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello world! x2');
+  res.send('Hello world!');
 });
 
 app.listen(3000, () => {
